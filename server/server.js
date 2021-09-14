5fabc0703bb74796472f29953f1881c8622939cf
Teggoon <daniel.hc.li@gmail.com>
2021-09-14 14:11:39 -0700

    major folder structure improvement

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require("path");
 app.use('/static', express.static(path.join(__dirname, '../public')))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 var io = require('socket.io')(http);
